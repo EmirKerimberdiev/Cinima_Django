ca18c09b47f31ec00d8ba6a015d642a1447a0fd8
EmirKerimberdiev <keremberdievemir43@gmail.com>
2024-11-05 17:30:09 +0600

    add some ditailes

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Films(models.Model):
+    GENRE_CHOICE = (
+        ('Ужасы', 'Ужасы'),
+        ('Комедия', 'Комедия'),
+        ('Боевик', 'Боевик')
+    )
+    image = models.ImageField(upload_to='film/', verbose_name='Загрузите картинку')
+    title = models.CharField(max_length=100, verbose_name='Укажите название фильма')
+    price = models.FloatField(verbose_name='Укажите цену фильма')
+    start_film = models.DateField(verbose_name='Укажите дату выхода фильма')
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICE,default='Комедия', verbose_name='Укажите жанр фильма')
+
+    time_ = models.TimeField(verbose_name='Укажите время продолжительнысти фильма')
+    director = models.CharField(max_length=30, verbose_name='Укажите режиссер фильма')
+    trailer = models.URLField(verbose_name='Укажите ссылку на трейлер фильма')
